8a58e587fc00891b6922ccab0046c0ca9990488e
LiYanan2004 <1474700628@qq.com>
2019-12-21 10:36:48 +0800

    test

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftUIKit",
-            dependencies: []),
+            dependencies: ["SwiftUIKit"]),
         .testTarget(
             name: "SwiftUIKitTests",
             dependencies: ["SwiftUIKit"]),
