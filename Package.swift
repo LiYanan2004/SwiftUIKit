9dffc3729e6ae088ed09a90c8dcb210f08367e6b
LiYanan2004 <1474700628@qq.com>
2020-01-20 09:10:38 +0800

    fix the issue " No such Module 'Combine' "

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIKit",
+    platforms: [SupportedPlatform.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -20,10 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftUIKit",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftUIKitTests",
-            dependencies: ["SwiftUIKit"]),
+            dependencies: [])
     ],
     swiftLanguageVersions: [SwiftVersion.v5]
 )
--- Sources/SwiftUIKit/Publishers.swift
+++ Sources/SwiftUIKit/Publishers.swift
@@ -5,11 +5,11 @@
 //  Created by LiYanan2004 on 2020/1/19.
 //
 
+/// Important: You also need to add `@Environment(\.colorScheme)` to receive the notifications
+
 import SwiftUI
 import Combine
 
-
-/// Important: You also need to add `@Environment(\.colorScheme)` to receive the notifications
 @available(iOS 13.0, *)
 public var colorSchemeDidChange: AnyPublisher<UIUserInterfaceStyle, Never> {
     UITraitCollection.current.publisher(for: \.userInterfaceStyle)
